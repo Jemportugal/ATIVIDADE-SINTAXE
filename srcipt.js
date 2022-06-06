898ccd55a51228da7d1282a08e0d2e9c0fc680e3
Jessica Portugal <jessicamendesdeportugal@gmail.com>
2022-06-06 17:44:43 -0300

    complemento

--- srcipt.js
+++ srcipt.js
@@ -1,24 +1,41 @@
 function numeros (x,  y) {
     const fraseUm = primeiraFrase (x, y);
     const fraseDois = segundaFrase (x, y);
- 
-function primeiraFrase (x, y)
+}
+
+function primeiraFrase (x, y){
     
+    let iguais ='';
     if ('x === b && y === b');
     return console.log('São iguais')
-
-function segundaFrase (x, y)
-    
-    if ('x+y = || > 10')
-    return console.log("Maior que 10");
- 
-    if ('x+y - || < 10')
-    return console.log("Menor que 10");
 }
+
+function segundaFrase (x, y){
+
+    const soma = x + y;
+
+    let comp10 = soma > 10;
+    let comp0 = soma < 10;
+    let comp20 = soma < 20;
+
+    if(comp10) {
+        resultado = 'Maior que 10';
+    }
+
+    if(comp20) {
+        resultado = 'Menor que 20'
+    }
+
+    if(comp0) {
+        resultado = 'Menor que 10';
+    }
+}
+
+    
+
 
 //if (numeros && number === iguais)
 //return('Numero são iguais');
-
 //if (numeros + number > 10)
 // return('Soma é maior ou igual a 10');
 
