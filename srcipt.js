92ef9ffd3a48ba14b42705946c396b3123830cf6
Jessica Portugal <jessicamendesdeportugal@gmail.com>
2022-06-06 17:32:39 -0300

    atividade sintaxe

---
+++ srcipt.js
@@ -0,0 +1,30 @@
+function numeros (x,  y) {
+    const fraseUm = primeiraFrase (x, y);
+    const fraseDois = segundaFrase (x, y);
+ 
+function primeiraFrase (x, y)
+    
+    if ('x === b && y === b');
+    return console.log('São iguais')
+
+function segundaFrase (x, y)
+    
+    if ('x+y = || > 10')
+    return console.log("Maior que 10");
+ 
+    if ('x+y - || < 10')
+    return console.log("Menor que 10");
+}
+
+//if (numeros && number === iguais)
+//return('Numero são iguais');
+
+//if (numeros + number > 10)
+// return('Soma é maior ou igual a 10');
+
+//if (numeros + number < 10)
+// return('Soma é menor ou igual a 10');
+
+
+
+
